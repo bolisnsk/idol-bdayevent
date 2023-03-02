a624d6ce6c11fd99c9cb801eeacb14eb8fbb494a
왕태현 <tfc574750@gmail.com>
2023-03-02 23:06:34 +0900

    2023-02-27 : Draft

--- idolsite/settings.py
+++ idolsite/settings.py
@@ -52,6 +52,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'idolsite.urls'
 
 TEMPLATES = [
