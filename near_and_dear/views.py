57a7171988b783f9bbd17f91445b4457fd2a7456
왕태현 <tfc574750@gmail.com>
2023-03-06 21:09:22 +0900

    Custom Pagination add

--- near_and_dear/views.py
+++ near_and_dear/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from near_and_dear.models import Category, Post
 from near_and_dear.serializers import CategorySerializer, PostListSerializer, PostSerializer
@@ -19,10 +20,13 @@ class CategoryAPIGenerics(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class PostResultsSetPagination(PageNumberPagination):
+    page_size = 4
 
 class PostListAPIGenerics(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    pagination_class = PostResultsSetPagination
     filter_backends = [SearchFilter, DjangoFilterBackend]
     filterset_fields = ['id', 'category']
     search_fields = ['title', 'address']
