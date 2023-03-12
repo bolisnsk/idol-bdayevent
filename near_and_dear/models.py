516b29f8ff4830f63ec4b420f284c6fe449b58be
권오은 <peachmoch2@gmail.com>
2023-03-12 16:43:34 +0900

    idol event 2

---
+++ near_and_dear/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=128)
+    content = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='post/', default='default.png')
+    create_dt = models.DateTimeField(auto_now_add=True)
+    update_dt = models.DateTimeField(auto_now=True)
+    start_day = models.DateField()
+    end_day = models.DateField()
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    class Meta:
+        ordering = ['id']
---
+++ process_data.py
@@ -0,0 +1,30 @@
+import csv
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from near_and_dear.models import Post
+
+CSV_PATH = './birthday_event.csv'
+
+with open(CSV_PATH, encoding='utf-8-sig') as csvfile:
+    data_reader = csv.reader(csvfile)
+    next(data_reader, None)
+    for row in data_reader:
+        Post.objects.create(
+            title = row[0],
+            address = row[1],
+            content = row[2],
+            url = row[3],
+            image = row[4],
+            create_dt = row[5],
+            update_dt = row[6],
+            start_day = row[7],
+            end_day = row[8],
+            open_time = row[9],
+            close_time = row[10],
+            category_id = int(row[11])
+        )
+        print(row)
