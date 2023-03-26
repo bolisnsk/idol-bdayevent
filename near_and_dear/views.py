eb79a056663dea3cd8688538c2ad0bd55156532c
왕태현 <tfc574750@gmail.com>
2023-03-26 23:10:04 +0900

    2023-03-26 : SearchAlgorithm

--- near_and_dear/views.py
+++ near_and_dear/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from fuzzywuzzy import fuzz
 
 from near_and_dear.models import Category, Post
 from near_and_dear.serializers import CategorySerializer, PostListSerializer, PostSerializer
@@ -29,7 +30,25 @@ class PostListAPIGenerics(ListCreateAPIView):
     pagination_class = PostResultsSetPagination
     filter_backends = [SearchFilter, DjangoFilterBackend]
     filterset_fields = ['id', 'category']
-    search_fields = ['title', 'address']
+    search_fields = ('title', 'address')
+
+    def filter_queryset(self, queryset):
+        search = self.request.query_params.get('search', None)
+
+        def sort_by_token_ratio(post):
+            titleScore = fuzz.token_set_ratio(search, post.title)*1.2
+            addressScore = fuzz.token_set_ratio(search, post.address)*10
+            return titleScore, addressScore
+
+        if search:
+            queryset = super().filter_queryset(queryset)
+            queryset = sorted(queryset, key=sort_by_token_ratio, reverse=True)
+            for number in queryset:
+                score = max(sort_by_token_ratio(number))
+                print(f"검색어: {search} | 게시물: {number.title} | 주소: {number.address} | 점수: {score}")
+#   Lamda - queryset = sorted(queryset, key=lambda post: fuzz.token_set_ratio(search, post.title), reverse=True)
+            queryset = [post for post in queryset if max(sort_by_token_ratio(post)) >= 100]
+        return queryset
 
 
 class PostAPIGenerics(RetrieveUpdateDestroyAPIView):
