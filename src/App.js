e48ad68e73a1da0aa4939d03755809130c43e2e2
bolisnsk <50792699+bolisnsk@users.noreply.github.com>
2023-05-30 02:48:50 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'animate.css';
-import List from './components/Layout/List';
 import Loading from './components/Layout/Loading';
 import { useMediaQuery } from 'react-responsive';
 import Mouse from './components/Layout/mouse';
@@ -13,6 +12,11 @@ import Header from './components/Layout/Header/Header';
 import axios from 'axios';
 import EventList from './components/Layout/EventList';
 import { positions } from '@mui/system';
+import Categories from './components/Layout/Categories';
+import Ctest from './components/Layout/ctest';
+import Banner from './components/Layout/Banner/Banner';
+import Exh from './components/Layout/Exh';
+import Cevent from './components/Layout/Cevent';
 
 function App() {
   const [xy,setXY]=useState({x:0,y:0})
@@ -23,13 +27,15 @@ const handleMouseMove=(e)=>{
  
   return (
     <div>
-      
-      
         <Routes>
         <Route path="/" element={<Header />}/>
-        <Route path="Search" element={<Search />} />
-        <Route path="Search/:word" element={<Search />} />
-        <Route path="Search/:word/detail/:title" element={<EventList />} />
+        <Route path="Search/:search" element={<Search />} />
+        <Route path="Search/:category" element={<Search />} />
+        <Route path="Search/:search/cafe" element={<Ctest />} />
+        <Route path="Search/:search/exhibition" element={<Exh />} />
+        <Route path="Search/:search/event" element={<Cevent />} />
+        <Route path="Search/:search/detail/:title" element={<EventList />} />
+     
       </Routes>
       
       </div>
