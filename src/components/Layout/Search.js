7285de50e77754c27ca165633028b6af692ead36
bolisnsk <50792699+bolisnsk@users.noreply.github.com>
2023-11-02 18:07:43 +0900

    Navbar 추가 및 css 수정

--- src/components/Layout/Search.js
+++ src/components/Layout/Search.js
@@ -5,20 +5,20 @@ import { useLocation } from "react-router";
 import './Search.css'
 import Mouse from "./mouse";
 import List from "./List";
-import Logo from "./Logo";
+import Navbar from "./navBar";
 
 
 function Search() {
     const location = useLocation();
     const { search } = location.state;
-    
+
     return (
         <div className="box">
             <Mouse/>
-            <header className='title'>
-              <Logo/>
+            <Navbar/>
+            <header>
+                <span className="header"> ❤ { search } 's EVENT ❤</span>
             </header>
-            <h2 className="header"> { search } 's EVENT</h2>
             <div className="content">
                 <main className="list">
                     <div className="ctitle">
